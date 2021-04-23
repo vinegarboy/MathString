c51c5d478010ab3895f8608663d7332e7c6fbc95
vinegarboy <necogusa398@gmail.com>
2021-04-23 09:10:01 +0900

    引き算関連処理の整理

--- TestProject/Class1.cs
+++ TestProject/Class1.cs
@@ -5,7 +5,6 @@ namespace MathString
 {
     public class Calculation
     {
-        private int ctoi(char c){return int.Parse(c.ToString());}
         public string Add(string date1,string date2){
             string ab = "";
             char[] _sd1 = date1.ToCharArray(),_sd2 = date2.ToCharArray(),_d1= new char[_sd1.Length],_d2= new char[_sd2.Length];
@@ -111,8 +110,8 @@ namespace MathString
             string value ="";
             if(date1.Length==date2.Length){
                 int hl = 0;//0 =,1 high,2 low
-                for(int i =date1.Length;i>=0;i--){
-                    if(int.Parse(date1[i].ToString())==int.Parse(date2[i].ToString())){
+                for(int i =date1.Length-1;i>=0;i--){
+                    if(int.Parse(date1[i].ToString())!=int.Parse(date2[i].ToString())){
                         if(int.Parse(date1[i].ToString())>int.Parse(date2[i].ToString())){
                             hl=1;
                             break;
@@ -126,12 +125,12 @@ namespace MathString
                 string[] d3 = new string[d1.Length];
                 if(hl==1){
                     for(int i=0;i<date1.Length;i++){
-                        d3[i] = $"{ctoi(d1[i])-ctoi(d2[i])}";
+                        d3[i] = $"{int.Parse(d1[i].ToString())-int.Parse(d2[i].ToString())}";
                     }
                     value = d3.ToString();
                 }else if(hl==2){
                     for(int i=0;i<date1.Length;i++){
-                        d3[i] = $"{ctoi(d2[i])-ctoi(d1[i])}";
+                        d3[i] = $"{int.Parse(d2[i].ToString())-int.Parse(d1[i].ToString())}";
                     }
                     value = d3.ToString();
                 }else{
@@ -139,41 +138,50 @@ namespace MathString
                 }
             }else if(date1.Length>date2.Length){
                 int sa = date1.Length-date2.Length;
-                char[] d1 = date1.ToCharArray(),d2 = date2.ToCharArray();
-                string[] d3 = new string[d1.Length];
+                char[] d1 = date1.ToCharArray(),d2 = new char[d1.Length];
                 d1=date1.ToCharArray();
                 for(int i = 0;i<date1.Length;i++){
-                    if(sa-1<=i){
+                    if(sa>=i+1){
                         d2[i] ='0';
                     }else{
                         d2[i] = date2.ToCharArray()[i];
                     }
                 }
                 for(int i=0;i<date1.Length;i++){
-                        d3[i] = $"{ctoi(d1[i])-ctoi(d2[i])}";
-                    }
-                    value = d3.ToString();
+                        value += $"{int.Parse(d1[i].ToString())-int.Parse(d2[i].ToString())}";
+                }
             }else if(date1.Length<date2.Length){
                 int sa = date2.Length-date1.Length;
-                char[] d1 = date1.ToCharArray(),d2 = date2.ToCharArray();
-                string[] d3 = new string[d1.Length];
+                char[] d2 = date2.ToCharArray(),d1 = new char[d2.Length];
                 d2=date2.ToCharArray();
                 for(int i =0;i<date2.Length;i++){
-                    if(sa-1<=i){
+                    if(sa>=i+1){
                         d1[i] = '0';
                     }
                     else{
-                        d1[i] = date1.ToCharArray()[i];
+                        d1[i] = date1.ToCharArray()[i-sa];
                     }
                 }
-                for(int i=0;i<date1.Length;i++){
-                        d3[i] = $"{ctoi(d2[i])-ctoi(d1[i])}";
+                for(int i=0;i<date2.Length;i++){
+                    int giv = int.Parse(d2[i].ToString())-int.Parse(d1[i].ToString());
+                    if(giv==0){
+                        value+=$"0";
+                    }else if(giv<0){
+                        value+="0";
+                        char[] _v = value.ToCharArray();
+                        _v[i-1] = Convert.ToChar(int.Parse(_v[i-1].ToString())-1);
+                        _v[i] = Convert.ToChar(10+giv);
+                        value = new string(_v);
+                    }else{
+                        value+= $"{giv}";
                     }
-                    value = d3.ToString();
+                }
+                value = "-"+value;
             }
             return value;
         }
         public string Multiplication(string date1,string date2){
+            Calculation ca = new Calculation();
             string value="";
             char[] _d1 = date1.ToCharArray(),_d2 =date2.ToCharArray();
             char[] d1 = new char[date1.Length],d2 = new char[date2.Length];
@@ -196,11 +204,11 @@ namespace MathString
                     for(int p = 0;p<i;p++){
                         az+="0";
                     }
-                    d4.Add((ctoi(d1[v])*ctoi(d2[i])).ToString()+az);
+                    d4.Add((int.Parse(d1[v].ToString())*int.Parse(d2[i].ToString())).ToString()+az);
                 }
                 for(int n =0;n<d4.Count;n++){
                     if(n!=0){
-                        d3[i]+=Add(d3[i],d4[n]);
+                        d3[i]+=ca.Add(d3[i],d4[n]);
                     }
                     else{
                         d3.Add(d4[n]);
@@ -209,7 +217,7 @@ namespace MathString
                 d3[i]+=z;
             }
             for(int i =0;i<d3.Count-1;i++){
-                value=Add(value,d3[i]);
+                value=ca.Add(value,d3[i]);
             }
             return value;
         }
--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -8,7 +8,7 @@ namespace TestProject
         static void Main(string[] args)
         {
             Calculation ca = new Calculation();
-            Console.WriteLine($"足し算:5+4={ca.Add("5","4")}\n掛け算:500*400={ca.Multiplication("500","400")}");
+            Console.WriteLine($"足し算:5000+400={ca.Add("5000","400")}\n引き算:500-50={ca.Subtraction("50","500")}\n掛け算:500*400={ca.Multiplication("500","400")}");
             Console.WriteLine("終了するにはキーを押してください。");
             Console.ReadKey();
         }
